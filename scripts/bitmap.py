bf16b48fe309cd799b48cc147be663d46778cf9d
Antoine Gruzelle <antogruz@gmail.com>
2018-11-18 14:53:50 +0100

    Extract bits functions

--- scripts/bitmap.py
+++ scripts/bitmap.py
@@ -2,19 +2,7 @@
 
 import bitsHandling
 import re
-from struct import *
-
-def read_int(fh):
-    return unpack("I", fh.read(4))[0]
-
-def read_short(fh):
-    return unpack("H", fh.read(2))[0]
-
-def write_int(fh, i):
-    fh.write(pack("I", i))
-
-def write_short(fh, s):
-    fh.write(pack("H", s))
+from bits import *
 
 class PictureHeader:
     def __init__(self):
